99dbb0518b0b320237ec8d25b2e02f10dbf46e1e
Thomas Bruun <thomas.bruun@gmail.com>
2011-04-11 20:10:02 +0200

    Fixed settings to maek postgres work in prod! And upgraded to django 1.3

--- src/development.py
+++ src/development.py
@@ -1,3 +1,15 @@
 from settings import *
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': ROOTPATH+'/project.db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+
 DEBUG = True
--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,9 @@
 # Django settings for littomsit project.
 import os
+import sys
+
 ROOTPATH = os.path.dirname(__file__)
+
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
@@ -10,17 +13,6 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': ROOTPATH+'/project.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
