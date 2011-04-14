1c5b0f8f23503c1f388a14fecbc020003fe7dd00
Thomas Bruun <thomas.bruun@gmail.com>
2011-04-14 18:09:56 +0200

    Removed the password from the production settings, you sneaky bastards

--- src/production.py
+++ src/production.py
@@ -1,11 +1,15 @@
 from settings import *
+import sys
+
+with open('/etc/sitdinner/db-password', 'rb') as f:
+    db_password = f.readline()
 
 DEBUG = True
 
 DATABASE_ENGINE = 'django.db.backends.postgresql_psycopg2'    # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'sitdinner'
 DATABASE_USER = 'sitdinner'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'thomasbruun'
+DATABASE_PASSWORD = db_password
 
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
