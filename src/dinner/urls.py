d0ecc9bfd39f9edf411b0a21c8136f437a432cc5
Thomas Bruun <thomas.bruun@gmail.com>
2011-05-23 00:10:59 +0200

    Added a front page with FAQ

--- src/dinner/urls.py
+++ src/dinner/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls.defaults import patterns, include
 
 urlpatterns = patterns('src.dinner.views',
-    (r'^cafeterias/$', 'get_cafeterias'),
-    (r'^cafeteria/(?P<cafeteria>\w+)/today/$', 'get_dinner_for_today'),
-    (r'^cafeteria/(?P<cafeteria>\w+)/(?P<year>\d{4})-(?P<month>\d{1,2})-(?P<day>\d{1,2})/$', 'get_dinner_for_day'),
+    (r'^$', 'front'),
+    (r'^api/cafeterias/$', 'get_cafeterias'),
+    (r'^api/cafeteria/(?P<cafeteria>\w+)/today/$', 'get_dinner_for_today'),
+    (r'^api/cafeteria/(?P<cafeteria>\w+)/(?P<year>\d{4})-(?P<month>\d{1,2})-(?P<day>\d{1,2})/$', 'get_dinner_for_day'),
 
-    (r'^cafeteria/(?P<cafeteria>\w+)/this_week/$', 'get_dinner_for_this_week'),
-    (r'^cafeteria/(?P<cafeteria>\w+)/weekly/(?P<year>\d{4})-(?P<month>\d{1,2})-(?P<day>\d{1,2})/$', 'get_dinner_for_week_by_day'),     # Example usage: api/cafeteria/Realfag/2011-3-7/
+    (r'^api/cafeteria/(?P<cafeteria>\w+)/this_week/$', 'get_dinner_for_this_week'),
+    (r'^api/cafeteria/(?P<cafeteria>\w+)/weekly/(?P<year>\d{4})-(?P<month>\d{1,2})-(?P<day>\d{1,2})/$', 'get_dinner_for_week_by_day'),     # Example usage: api/cafeteria/Realfag/2011-3-7/
 )
--- src/dinner/views.py
+++ src/dinner/views.py
@@ -5,12 +5,15 @@ import re
 import time
 
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render_to_response
 from django.core import serializers
 from django.utils import simplejson as json
 
 from dinner.models import Cafeteria, Dinner
 from datetime import date, timedelta
+
+def front(request):
+    return render_to_response('dinner/front.html')
 
 def get_cafeterias(request):
     
